1d988291ca00207f8d6f3a445491bbff8c702b97
Assppex <joom361@gmail.com>
2025-12-15 14:34:42 +0300

    fix base path

--- Program.cs
+++ Program.cs
@@ -2,23 +2,23 @@ using cros_dots_hw_blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Добавляем поддержку интерактивных серверных компонентов
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UsePathBase("/dotnet_blazor_app");
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
+app.UseStaticFiles();       // ← должен идти ПОСЛЕ UsePathBase, но ДО UseRouting
+app.UseRouting();           // ← явно добавляем (иногда требуется)
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
